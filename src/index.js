0eb7611dd0dcb564300b07ae3f8d9a186cf129ac
Ben Ellis <ben.ellis@softweyr.co.uk>
2019-03-05 22:14:37 +0000

    Added Select changes() to see if it returns the number of affected rows.

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ class KeyvSql extends EventEmitter {
 		      this.entry.key.equals(key).and(this.entry.value.equals(serializedOriginalValue))
 		    ).toString();
 			} else {
-				upsert = `REPLACE INTO "keyv" ("key", "value") SELECT '${key}', '${serializedValue}' FROM "keyv" WHERE "keyv"."key" = '${key}' AND "keyv"."value" = '${serializedOriginalValue}';`;
+				upsert = `REPLACE INTO "keyv" ("key", "value") SELECT '${key}', '${serializedValue}' FROM "keyv" WHERE "keyv"."key" = '${key}' AND "keyv"."value" = '${serializedOriginalValue}'; SELECT changes();`;
 				/*
 				upsert = this.entry.replace({ key, value: serializedValue }).select().where(
 		      this.entry.key.equals(key).and(this.entry.value.equals(serializedOriginalValue))
